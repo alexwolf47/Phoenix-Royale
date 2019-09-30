178beb2032cdc263955e5923581d87564de151f1
Alex Wolf <alexwolf47@gmail.com>
2019-09-30 13:30:06 -0700

    Fixed broken about page

--- lib/phoenix_royale_web/live/about_live.ex
+++ lib/phoenix_royale_web/live/about_live.ex
@@ -4,8 +4,8 @@ defmodule PhoenixRoyaleWeb.AboutLive do
   @doc """
   Why is this a LiveView controller? I was going to add some animations and effects to this page. I ran out of time :(
   """
-  def render(_assigns) do
-    Phoenix.View.render(PhoenixRoyaleWeb.AboutView, "about.html")
+  def render(assigns) do
+    Phoenix.View.render(PhoenixRoyaleWeb.AboutView, "about.html", assigns)
   end
 
   def mount(_session, socket) do
